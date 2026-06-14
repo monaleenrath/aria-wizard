a588fa7d81cdb157ab441f8ef3258e6955040485
monaleenrath <monaleenrath1401@gmail.com>
2026-06-14 10:32:38 -0400

    chore: agent/html_generator.py

--- agent/html_generator.py
+++ agent/html_generator.py
@@ -1414,7 +1414,7 @@ function ariaFilter(sel) {{
     )
 
     return f"""<!DOCTYPE html>
-<!-- ARIA_DEPLOY_VERSION=2026-06-13-v18 | {_diag} -->
+<!-- ARIA_DEPLOY_VERSION=2026-06-13-v19 | {_diag} -->
 <html>
 <head>
 <meta charset="utf-8">
